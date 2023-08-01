ef317dd8856a8112d52d539a3ca0535b61531212
focusotter <mtliendo@focusotter.com>
2023-08-01 11:50:43 -0500

    Initial commit

---
+++ test/appsync-l3-postconf.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncL3Postconf from '../lib/appsync-l3-postconf-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-l3-postconf-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncL3Postconf.AppsyncL3PostconfStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
